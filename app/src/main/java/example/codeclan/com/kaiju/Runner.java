3e6163478a44f89df9f1826e9dbd659dcd6f808d
Jamie Sime <jamiesime@gmail.com>
2017-10-31 16:17:12 +0000

    added runner

---
+++ app/src/main/java/example/codeclan/com/kaiju/Runner.java
@@ -0,0 +1,19 @@
+package example.codeclan.com.kaiju;
+
+/**
+ * Created by user on 31/10/2017.
+ */
+
+public class Runner {
+
+
+
+    public static void main(String[] args){
+        Onionsaur onionsaur = new Onionsaur("Ricky", 1000, 75);
+        Tomatozilla tomatozilla = new Tomatozilla("Gregory", 800, 125);
+        Tricycle tricycle = new Tricycle("Rusty", 12);
+        ImperviousTank tank = new ImperviousTank("Grace", 20000);
+        
+    }
+
+}
